e460c26b4a2ab73be40792dc7b4ae3269ced162d
elvesbd <elvesbd@yahoo.com>
2023-06-22 09:26:11 -0300

    feat: create PostMap

---
+++ Data/Mappings/PostMap.cs
@@ -0,0 +1,57 @@
+using Blog.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Blog.Data.Mappings
+{
+    public class PostMap : IEntityTypeConfiguration<Post>
+    {
+        public void Configure(EntityTypeBuilder<Post> builder)
+        {
+            builder.ToTable("User");
+            builder.HasKey(p => p.Id);
+            builder.Property(p => p.Id)
+                .ValueGeneratedOnAdd()
+                .UseIdentityColumn();
+
+            builder.Property("Title")
+                .IsRequired()
+                .HasColumnName("Title")
+                .HasColumnType("VARCHAR")
+                .HasMaxLength(160);
+
+            builder.Property("Summary")
+                .IsRequired()
+                .HasColumnName("Summary")
+                .HasColumnType("VARCHAR")
+                .HasMaxLength(255);
+
+            builder.Property("Body")
+                .IsRequired()
+                .HasColumnName("Body")
+                .HasColumnType("TEXT");
+
+            builder.Property("Slug")
+                .IsRequired()
+                .HasColumnName("Slug")
+                .HasColumnType("VARCHAR")
+                .HasMaxLength(80);
+
+            builder.Property("CreateDate")
+                .IsRequired()
+                .HasColumnName("CreateDate")
+                .HasColumnType("SMALLDATETIME")
+                .HasDefaultValueSql("GETDATE()");
+            // .HasDefaultValue(DateTime.Now.ToUniversalTime());
+
+            builder.Property("LastUpdateDate")
+                .IsRequired()
+                .HasColumnName("LastUpdateDate")
+                .HasColumnType("SMALLDATETIME")
+                .HasDefaultValue(DateTime.Now.ToUniversalTime());
+
+            builder.HasIndex(p => p.Slug, "IX_Post_Slug")
+                .IsUnique();
+        }
+    }
+}
