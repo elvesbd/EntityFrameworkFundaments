ecd21765b52956916beefcd9b1c000e20f0fe19f
elvesbd <elvesbd@yahoo.com>
2023-06-22 10:06:14 -0300

    feat: config BlogDataContext with data mappings

--- Data/BlogDataContext.cs
+++ Data/BlogDataContext.cs
@@ -1,3 +1,4 @@
+using Blog.Data.Mappings;
 using Blog.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,10 +10,21 @@ namespace Blog.Data
             Server=localhost,1433;Database=Blog;User ID=sa;Password=1q2w3e4r@#$;Encrypt=false
         ";
 
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<User> Users { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder options)
         {
             options.UseSqlServer(CONNECTION_STRING);
             // options.LogTo(Console.WriteLine); habilitas os logs das consultas no banco
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CategoryMap());
+            modelBuilder.ApplyConfiguration(new UserMap());
+            modelBuilder.ApplyConfiguration(new PostMap());
         }
     }
 }
