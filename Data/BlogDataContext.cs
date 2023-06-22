3367366300d2512c1b9740c20f83c7692638374c
elvesbd <elvesbd@yahoo.com>
2023-06-22 08:37:22 -0300

    refactor: adjusting project

--- Data/BlogDataContext.cs
+++ Data/BlogDataContext.cs
@@ -9,10 +9,6 @@ namespace Blog.Data
             Server=localhost,1433;Database=Blog;User ID=sa;Password=1q2w3e4r@#$;Encrypt=false
         ";
 
-        public DbSet<Category>? Categories { get; set; }
-        public DbSet<Post>? Posts { get; set; }
-        public DbSet<User>? Users { get; set; }
-
         protected override void OnConfiguring(DbContextOptionsBuilder options)
         {
             options.UseSqlServer(CONNECTION_STRING);
--- Models/Post.cs
+++ Models/Post.cs
@@ -3,49 +3,19 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Blog.Models
 {
-    [Table("Post")]
     public class Post
     {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
-
-        [Required]
-        [MaxLength(160)]
-        [Column("Title", TypeName = "VARCHAR")]
         public string? Title { get; set; }
-
-        [Required]
-        [MinLength(5)]
-        [MaxLength(255)]
-        [Column("Summary", TypeName = "VARCHAR")]
         public string? Summary { get; set; }
-
-        [Required]
-        [Column("Body", TypeName = "TEXT")]
         public string? Body { get; set; }
-
-        [Required]
-        [MinLength(1)]
-        [MaxLength(80)]
-        [Column("Slug", TypeName = "VARCHAR")]
         public string? Slug { get; set; }
-
-        [DataType(DataType.Date)]
-        [Column("CreateDate", TypeName = "DATETIME")]
         public DateTime CreateDate { get; set; }
-
-        [DataType(DataType.Date)]
-        [Column("LastUpdateDate", TypeName = "DATETIME")]
         public DateTime LastUpdateDate { get; set; }
 
-        [ForeignKey("CategoryId")]
-        public int CategoryId { get; set; }
         public Category? Category { get; set; }
-
-        [ForeignKey("AuthorId")]
-        public int AuthorId { get; set; }
         public User? Author { get; set; }
+        public List<Tag>? Tags { get; set; }
 
     }
 }
